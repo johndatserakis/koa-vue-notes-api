1dd2308009fe681394a91961f31c86b3a93a937b
John Datserakis <johndatserakis@gmail.com>
2017-08-28 23:52:32 -0400

    test testing

--- tests/test.js
+++ tests/test.js
@@ -9,6 +9,12 @@ const request = axios.create({ baseURL: url })
 //Grab the db variable
 import db from '../src/db/db'
 
+// //Before all promosie fix for async/await Jest
+// //https://github.com/facebook/jest/issues/1760
+// beforeAll(() => {
+//     global.Promise = require.requireActual('promise');
+// });
+
 //After all the tests are done we're going to close our server
 //and rollback our database.
 afterAll(async () => {
@@ -25,33 +31,34 @@ describe('general actions', () => {
         expect.assertions(1)
         const response = await request.get('/')
         expect(response.status).toBe(200)
+        Promise.resolve()
     });
 });
 
-describe('user account actions', () => {
-    beforeAll(async () => {
-        await db.migrate.rollback()
-        await db.migrate.latest()
-    });
+// describe('user account actions', () => {
+//     beforeAll(async () => {
+//         await db.migrate.rollback()
+//         await db.migrate.latest()
+//     });
 
-    it('signs up a user', async () => {
-        expect.assertions(1)
-        const response = await request.post('/api/v1/user/signup', {
-            "firstName": "TestFirstName",
-            "lastName": "TestLastName",
-            "username": "TestUsername",
-            "email": "TestEmail@example.com",
-            "password": "TestPassword"
-        })
-        expect(response.status).toBe(200)
-    });
+//     it('signs up a user', async () => {
+//         expect.assertions(1)
+//         const response = await request.post('/api/v1/user/signup', {
+//             "firstName": "TestFirstName",
+//             "lastName": "TestLastName",
+//             "username": "TestUsername",
+//             "email": "TestEmail@example.com",
+//             "password": "TestPassword"
+//         })
+//         expect(response.status).toBe(200)
+//     });
 
-    it('authenticates a user', async () => {
-        expect.assertions(1)
-        const response = await request.post('/api/v1/user/authenticate', {
-            "username": "TestUsername",
-            "password": "TestPassword"
-        })
-        expect(response.status).toBe(200)
-    });
-})
+//     it('authenticates a user', async () => {
+//         expect.assertions(1)
+//         const response = await request.post('/api/v1/user/authenticate', {
+//             "username": "TestUsername",
+//             "password": "TestPassword"
+//         })
+//         expect(response.status).toBe(200)
+//     });
+// })
